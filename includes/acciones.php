6a1255949a1332f04303a2622691b38c28ca3bf8
juan <juan@Juank-PC>
2016-10-06 04:05:38 -0500

    eliminar terminado

--- includes/acciones.php
+++ includes/acciones.php
@@ -47,7 +47,7 @@ class acciones
     {
         $mñn = "7:03:00";
        include 'db_connect.php';
-        $consulta = "SELECT empleados.cedula as 'cedula',empleados.nombres as 'nombres', empleados.apellidos as 'apellidos',  entradas.fecha as 'fecha', entradas.hora as 'entrada', salidas.hora as 'salida' FROM entradas,salidas,empleados,dependencias where dependencias.id_depen = empleados.dependencias_id_depen and empleados.cedula = entradas.empleados_cedula and empleados.cedula = salidas.empleados_cedula and dependencias.id_depen = $id  by fecha desc;";
+        $consulta = "SELECT empleados.cedula as 'cedula',empleados.nombres as 'nombres', empleados.apellidos as 'apellidos',  entradas.fecha as 'fecha', entradas.hora as 'entrada', salidas.hora as 'salida' FROM entradas,salidas,empleados,dependencias where dependencias.id_depen = empleados.dependencias_id_depen and empleados.cedula = entradas.empleados_cedula and empleados.cedula = salidas.empleados_cedula and dependencias.id_depen = $id order by fecha desc;";
         $result   = $mysqli->query($consulta);
         echo '<table>';
          echo'<tr>';
@@ -62,11 +62,16 @@ class acciones
         while ($campo=mysqli_fetch_object($result)) 
         {
             echo "<tr><td>$campo->cedula</td><td>$campo->nombres</td><td>$campo->apellidos</td><td>$campo->fecha</td>";
+            $entrada = $campo->entrada;
             $entrada=$campo->entrada;
             $entrmañ=  strtotime("7:06");
             if($entrada>$entrmañ)
             {
-              echo " <td id=ok >$campo->entrada</td>";   
+                echo "<td id='ok' >$campo->entrada</td>";
+            }
+            else
+            {
+              echo "<td id=to >$campo->entrada</td>";  
             }
             echo "<td>$campo->salida</td></tr>";
         }
