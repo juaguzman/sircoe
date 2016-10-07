74f69daa3e51d9a6796117b0103cced458ea3240
juan <juan@Juank-PC>
2016-10-07 01:07:44 -0500

    busqueda

--- vistas/admin/nav.php
+++ vistas/admin/nav.php
@@ -12,18 +12,6 @@
                          <li><a href="buscarCedula.php">Busqueda Reportes</a></li>
                     </ul>
                 </li>
-                <li><a class="hsubs" href="#">Menu 3</a>
-                    <ul class="subs">
-                        <li><a href="#">Submenu 3-1</a></li>
-                        <li><a href="#">Submenu 3-2</a></li>
-                        <li><a href="#">Submenu 3-3</a></li>
-                        <li><a href="#">Submenu 3-4</a></li>
-                        <li><a href="#">Submenu 3-5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Menu 4</a></li>
-                <li><a href="#">Menu 5</a></li>
-                <li><a href="#">Menu 6</a></li>
                 <div id="nomses">
                     <li id="nomses" class="active" ><a href="#">¡Bienvenido, <?php if (isset($_GET['error'])){echo '<p class="error">Error Logging In!</p>';}else {echo htmlentities($_SESSION['username']);}?> !</a>
                         <ul class="subs">
@@ -31,7 +19,7 @@
                           <li><a href=register.php >Agegar Administrador</a></li>
                            <?php } ?>
                             <li><a href="modPerfil.php">Modificar Perfil</a></li>
-                            <li><a href="../includes/logout.php">Cerrar sesion</a></li>
+                            <li><a href="../../includes/logout.php">Cerrar sesion</a></li>
                     </ul>
                     </li>
                 </div>
